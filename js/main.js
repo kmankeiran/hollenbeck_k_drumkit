1b0ff848e10a31dc64300ef94a0402d8cf3d2932
Hollenbeck <k_hollenbeck@tss-hr420-dm19.fc.ca>
2019-03-04 13:20:05 -0500

    graphic part of the code

--- js/main.js
+++ js/main.js
@@ -12,11 +12,35 @@
 
 		// the return keyword stops javascript from running (any code below it)
 		if (!audio) { return; }
+		// audio == null
 		
 		//play the audio track
 		audio.currentTime = 0;
 		audio.play();
+
+		// select the parent div and animate it
+		//  try selecting the matching parent div element
+		let key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+		key.classList.add('playing');
+	}
+
+	function removePlayingClass(e) {
+		//listen for a CSS transition to finish and then reset the element
+		//by removing the playing class
+
+		if (e.propertyName !== "transform") {
+			return;
+		} else {
+			console.log('do some stuff here, transition is done');
+			e.currentTarget.classList.remove('playing');
+		}
 	}
 
 	window.addEventListener('keydown', playDrumSound);
+
+	// grab all of the keyboard keys that have the .key class applied
+	const keys = Array.from(document.querySelectorAll('.key'));
+
+	// add a transition end to each key so that we can reset it
+	keys.forEach(key => key.addEventListener('transitionend', removePlayingClass))
 })();
